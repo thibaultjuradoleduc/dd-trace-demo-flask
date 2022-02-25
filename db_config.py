aece2ef102da8fd0070174dd820221080cc61c8d
TiboJ <thibault.juradoleduc@gmail.com>
2022-02-25 09:43:41 +0100

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,6 +1,6 @@
 import os
 
-db_host = os.environ.get('DB_HOST', 'localhost')
-db_username = os.environ.get('DB_USERNAME', 'admin')
-db_password = os.environ.get('DB_PASSWORD', 'password')
-db_name = os.environ.get('DB_NAME', 'defaultdb')
+db_host = os.environ.get('DB_HOST', 'mysql')
+db_username = os.environ.get('DB_USERNAME', 'demo')
+db_password = os.environ.get('DB_PASSWORD', 'demo')
+db_name = os.environ.get('DB_NAME', 'demo')
